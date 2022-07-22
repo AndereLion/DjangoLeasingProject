d87e5b578b273cb98b87bbc6e63f29bc8ab9666a
AndereLion <senjaskorp@gmail.com>
2022-07-22 14:44:31 +0200

    ...

--- apps/calc/forms.py
+++ apps/calc/forms.py
@@ -6,3 +6,4 @@ class ReviewForm(forms.Form):
     car_model = forms.CharField(label="Car Model", max_length=50)
     car_year = forms.IntegerField(label="Car Year")
     car_firs_payment = forms.CharField(label="First Payment", max_length=10)
+    comment = forms.CharField(label="Comment", widget=forms.Textarea(attrs={'class': 'text_area'}))

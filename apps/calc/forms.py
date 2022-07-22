d8519430bc315f6b8c0fb9f04aa923bffa13eb03
AndereLion <senjaskorp@gmail.com>
2022-07-23 00:12:21 +0200

    ...

--- apps/calc/forms.py
+++ apps/calc/forms.py
@@ -1,9 +1,21 @@
 from django import forms
+from .models import CarRequest
 
 
-class ReviewForm(forms.Form):
-    car_brand = forms.CharField(label="Car Brand", max_length=50)
-    car_model = forms.CharField(label="Car Model", max_length=50)
-    car_year = forms.IntegerField(label="Car Year", max_value=2022, min_value=2011)
-    car_firs_payment = forms.IntegerField(label="First Payment", min_value=35000)
-    comment = forms.CharField(label="Comment", widget=forms.Textarea(attrs={'class': 'text_area'}))
+# class ReviewForm(forms.Form):
+#     car_brand = forms.CharField(label="Car Brand", max_length=50)
+#     car_model = forms.CharField(label="Car Model", max_length=50)
+#     car_year = forms.IntegerField(label="Car Year", max_value=2022, min_value=2011)
+#     car_firs_payment = forms.IntegerField(label="First Payment", min_value=35000)
+#     comment = forms.CharField(label="Comment", widget=forms.Textarea(attrs={'class': 'text_area'}))
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = CarRequest
+        fields = ['car_brand', 'car_model', 'car_year', 'car_first_payment', 'phone_number','email']
+        labels = {
+            'car_brand': "Brand",
+            'car_model': 'Model',
+            'car_year': 'Year',
+            'car_first_payment': "First payment"
+        }
--- apps/calc/models.py
+++ apps/calc/models.py
@@ -1,15 +1,19 @@
 from django.db import models
+from django.core.validators import validate_email, MaxValueValidator, MinValueValidator, MinLengthValidator, \
+    MaxLengthValidator
 
 
 # Create your models here.
 class CarRequest(models.Model):
     car_brand = models.CharField(max_length=50)
     car_model = models.CharField(max_length=50)
-    car_year = models.IntegerField()
-    car_firs_payment = models.IntegerField()
-    comment = models.CharField(max_length=300)
+    car_year = models.IntegerField(validators=[MinValueValidator(2011), MaxValueValidator(2022)])
+    car_first_payment = models.IntegerField(validators=[MinValueValidator(35000)])
+    phone_number = models.CharField(max_length=20, validators=[MinLengthValidator(13), MinLengthValidator(9)])
+    email = models.EmailField(validators=[validate_email])
     created_at = models.DateTimeField(auto_now_add=True, )
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'Brand: {self.car_brand}, Model: {self.car_model}, Year: {self.car_year}, First payment: {self.car_firs_payment}'
+        return f'Brand: {self.car_brand}, Model: {self.car_model}, Year: {self.car_year}, ' \
+               f'First payment: {self.car_first_payment}'

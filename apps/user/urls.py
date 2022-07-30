865d60df31c9709c14d454d5a848a9e796f775a8
AndereLion <senjaskorp@gmail.com>
2022-07-30 14:02:21 +0200

    user_created page

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import user_login_view, logout_user
+from .views import user_login_view, logout_user, user_sign_up_view, user_created_view
 
 app_name = 'user'
 urlpatterns = [
 
     path('login/', user_login_view, name='user_login'),
-    path('logout/', logout_user, name='user_logout')
+    path('logout/', logout_user, name='user_logout'),
+    path('sign_up/', user_sign_up_view, name='user_sign_up'),
+    path('user_created/', user_created_view, name='user_created')
 
 ]
--- apps/user/views.py
+++ apps/user/views.py
@@ -2,10 +2,12 @@ from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
 def user_login_view(request, *args, **kwargs):
+    page = 'user_login'
     if request.user.is_authenticated:
         return redirect('home:home')
     if request.method == 'POST':
@@ -27,3 +29,23 @@ def user_login_view(request, *args, **kwargs):
 def logout_user(request):
     logout(request)
     return redirect('user:user_login')
+
+
+def user_sign_up_view(request):
+    page = 'user_sign_up'
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            return redirect('user:user_created')
+
+    context = {'page': page, 'form': form}
+
+    return render(request, 'user/login_page.html', context)
+
+
+def user_created_view(request):
+    return render(request, 'user/user_created.html')
